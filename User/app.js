4a601e3fb2da4a6973fa7b84f21dbda55c29063a
Noam Shurki <shurkin@post.bgu.ac.il>
2024-12-20 18:22:11 +0200

    add DB container

--- User/app.js
+++ User/app.js
@@ -1,18 +1,23 @@
-const userApi = require("./api/userApi");
-console.log("Hello app");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const db = require('./config/db.js');
-
+const sequelize = require('../config/db');
+const userApi = require('./api/userApi');
+const User = require('./data/models/user'); // Import the User model
+// Import other models as needed
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to database
+// Connect to database and synchronize models
+sequelize.sync().then(() => {
+  console.log('Database synchronized');
+  // Start the server after the database is synchronized
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => {
+  console.error('Unable to synchronize the database:', err);
+});
 
 userApi(app);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
