14556755514ffbc5f001539758d71a71174a902e
roflmoqkz <roflmoqkz@gmail.com>
2025-01-01 12:02:52 +0200

    removed comment

--- Group/domain/validation.js
+++ Group/domain/validation.js
@@ -2,7 +2,7 @@ const e = require("express");
 
 const validate = async (accessToken) => {
     //call auth api and recive username if token is valid
-    // const response = await fetch('http://localhost:3000/auth/validate')
+    const response = await fetch('http://localhost:3000/auth/validate')
     userEmail = "placeholder@gmail.com"
     if (userEmail) {
         return {userEmail};
