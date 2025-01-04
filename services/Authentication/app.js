2213369774daac2ac85034b007f128b8a86511a7
Noam Shurki <shurkin@post.bgu.ac.il>
2025-01-04 14:08:19 +0200

    add console debug message in auth service

--- services/Authentication/app.js
+++ services/Authentication/app.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  console.log(`Auth service received request: ${req.method} ${req.url}`);
+  next();
+});
+
 const connectWithRetry = async () => {
   try {
     await db.sync();
