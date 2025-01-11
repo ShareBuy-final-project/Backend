2fd7a75d8ecad68ed0cabb9299e95b9012d721f9
Noam Shurki <shurkin@post.bgu.ac.il>
2025-01-11 15:21:19 +0200

    add debug messages

--- services/User/api/userApi.js
+++ services/User/api/userApi.js
@@ -47,6 +47,7 @@ module.exports = (app) => {
       }
 
       const { password, ...userData } = user.dataValues;
+      console.log('User data returned:', userData);
       res.status(200).json(userData);
     } catch (error) {
       if (error.message === 'Invalid token') {
--- services/User/domain/validation.js
+++ services/User/domain/validation.js
@@ -2,6 +2,7 @@ const e = require("express");
 const axios = require('axios');
 
 const validate = async (accessToken) => {
+    console.log(`Sending request to validate token: ${accessToken}`);
     try {
         const response = await axios.get('http://api-gateway/validate-token', {
             headers: {
@@ -10,9 +11,11 @@ const validate = async (accessToken) => {
         });
 
         if (response.status === 200) {
+            console.log('Token validated successfully');
             return { userEmail: response.data.data.email };
         }
     } catch (error) {
+        console.error('Error validating token:', error.message);
         throw new Error('Invalid token');
     }
 }
