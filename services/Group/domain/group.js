2929b896ec7c92016c0861defee1645ddb839b67
idonag <idonag@post.bgu.ac.il>
2025-01-24 15:33:28 +0200

    total amount in group

--- services/Group/domain/group.js
+++ services/Group/domain/group.js
@@ -4,6 +4,10 @@ const { Op } = require('sequelize');
 const axios = require('axios');
 require('dotenv').config();
 
+const getTotalAmount = async (id) =>{ 
+  await GroupUser.sum('amount', { where: { groupId: id, paymentConfirmed: true  } }
+)};
+
 const create = async ({ name, creator, description, image, price, discount, size, category, businessNumber }) => {
   if (!name || !price || !discount || !size || !category || !businessNumber) {
     throw new Error('Missing required fields');
@@ -12,6 +16,8 @@ const create = async ({ name, creator, description, image, price, discount, size
   // if (existingGroup) {
   //   throw new Error('Name already exists');
   // }
+
+
 
   try {
     const newGroup = await Group.create({
@@ -35,7 +41,7 @@ const create = async ({ name, creator, description, image, price, discount, size
 const getGroup = async (id) => {
   try {
     const group = await Group.findOne({where: {id} });
-    totalAmount =await toatalAmount(id);
+    totalAmount = await getTotalAmount(id);
     group.totalAmount = totalAmount;
     return group;
   } 
@@ -115,7 +121,7 @@ const searchGroups = async ({ filters, page, limit, userEmail }) => {
   console.log('savedGroupIds', savedGroupIds);
 
   const groupsWithTotalAmount = await Promise.all(groups.map(async group => {
-    const totalAmount = totalAmount(group.groupId);
+    const totalAmount = await getTotalAmount(group.groupId);
     const { description, category, creator, image, ...groupData } = group.toJSON();
 
     // Convert BLOB to base64 string if image exists
@@ -208,9 +214,7 @@ const getUserGroups = async (userEmail, page = 1, limit = 10) => {
   }
 };
 
-const toatalAmount = async (id) =>{ 
-  await GroupUser.sum('amount', { where: { groupId: id, paymentConfirmed: true  } }
-)};
+
 module.exports = {
   create, getGroup, saveGroup, joinGroup, leaveGroup, checkGroupExists, searchGroups, getBusinessHistory, getSavedGroups, getUserHistory, getUserGroups
 };
