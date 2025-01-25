62e37200f7dd5f5a04d298af7f5376820eded029
Noam Shurki <shurkin@post.bgu.ac.il>
2025-01-25 21:24:51 +0200

    increse

--- ApiGateway/app.js
+++ ApiGateway/app.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // Increase payload size limit to 10MB
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({limit: '10mb', extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 //app.use(bodyParser.urlencoded({ extended: true }));
 
--- services/Group/app.js
+++ services/Group/app.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Increase payload size limit to 10MB
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({limit: '10mb', extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 console.log('Starting Group service...');
 
