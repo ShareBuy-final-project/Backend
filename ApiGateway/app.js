b9350571c7a1941ab928a5db1c575e0e2c46dd26
Noam Shurki <shurkin@post.bgu.ac.il>
2025-03-22 17:45:53 +0200

    update chat service

--- ApiGateway/app.js
+++ ApiGateway/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const http = require('http');
 // const https = require('https');
 // const fs = require('fs');
 
