ce86ed50ad2a5884bc55ab436b2a1be86678affe
Noam Shurki <shurkin@post.bgu.ac.il>
2025-03-29 21:44:59 +0300

    update myChats

--- config/models/index.js
+++ config/models/index.js
@@ -9,6 +9,31 @@ const PrivateChat = require('./tables/privateChat');
 const Message = require('./tables/message');
 const sequelize = require('./db');
 
+// Define associations
+Group.hasMany(GroupUser, { foreignKey: 'groupId' });
+GroupUser.belongsTo(Group, { foreignKey: 'groupId' });
+
+Group.hasOne(GroupChat, { foreignKey: 'groupId' });
+GroupChat.belongsTo(Group, { foreignKey: 'groupId' });
+
+GroupChat.hasMany(Message, { foreignKey: 'groupId' });
+Message.belongsTo(GroupChat, { foreignKey: 'groupId' });
+
+User.hasMany(GroupUser, { foreignKey: 'userEmail' });
+GroupUser.belongsTo(User, { foreignKey: 'userEmail' });
+
+User.hasMany(Group, { foreignKey: 'creator' });
+Group.belongsTo(User, { foreignKey: 'creator' });
+
+Business.hasMany(Group, { foreignKey: 'businessNumber' });
+Group.belongsTo(Business, { foreignKey: 'businessNumber' });
+
+User.hasMany(PrivateChat, { foreignKey: 'userEmail' });
+PrivateChat.belongsTo(User, { foreignKey: 'userEmail' });
+
+Business.hasMany(PrivateChat, { foreignKey: 'businessNumber' });
+PrivateChat.belongsTo(Business, { foreignKey: 'businessNumber' });
+
 module.exports = {
   User,
   Group,
