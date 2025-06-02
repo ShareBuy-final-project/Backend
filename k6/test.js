92016540295e6f387d60d5c636853f2f92ed1f0d
roflmoqkz <roflmoqkz@gmail.com>
2025-06-02 21:46:57 +0300

    added more tests

--- k6/test.js
+++ k6/test.js
@@ -1,7 +1,9 @@
 import http from 'k6/http'
 import {SharedArray} from 'k6/data'
+import { response } from 'express';
 //const {faker} = require('@faker-js/faker')
 //const fs = require('node:fs');
+const baseURL = 'http://132.73.84.56/'
 export const options = {
     vus: 1,
     duration: '10s'
@@ -10,7 +12,10 @@ var data = new SharedArray('users',function() {
   return JSON.parse(open('../data.json'));
 })
 var index = 0;
-
+const num_users = 5; //can be up to 1000 as there are 1000 entries in data.json
+var tokens = [];
+for (let i = 0; i < num_users; i++)
+  tokens.push('');
 /*function generate_users_file() {
   var users = [];
   for (let i = 0; i < 1000; i++)
@@ -22,7 +27,7 @@ var index = 0;
   });
 }*/
 function generate_many_users() {
-  const url = 'http://132.73.84.56/userApi/register';
+  const url = baseURL+'userApi/register';
   const params = JSON.stringify({
     'Content-Type': 'application/json',
   });
@@ -44,9 +49,95 @@ function generate_many_users() {
         }
   http.post(url, payload, params);
 }
+function mass_login()
+{
+  const url = baseURL+'authApi/login';
+  const params = JSON.stringify({
+    'Content-Type': 'application/json',
+  });
+  
+  for (let i = 0; i < num_users; i++)
+  {
+    let element = data[i]
+    let email = element[1]
+    let password = element[2]
+    let payload = {
+            email: email,
+        password: password,
+        isBusiness: false
+          }
+    response = http.post(url, payload, params);
+    tokens[i] = response.body.accessToken;
+  }
+}
+var groupId = 304; //replace with valid group id
+function init_chat_test() {
+  mass_login();
+  const url = baseURL+'groupApi/joinGroup';
+  for (let i = 0; i < num_users; i++)
+  {
+    const accessToken = tokens[i]
+    const params = JSON.stringify({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    });
+    payload = {
+      groupId: id,
+        amount: 1
+    }
+     http.post(url, payload, params);
+  }
+}
+function chat_test() {
+  ws.connect(url, params, function (socket) {
+    for (let i = 0; i < num_users; i++) {
+      let element = data[i]
+      let email = element[1]
+      socket.sendMessage("i am user "+i,groupId,email);
+    }
+  });
+}
+function join_unjoin_test() {
+  const url = baseURL+'groupApi/joinGroup';
+  const url2 = baseURL+'groupApi/leaveGroup';
+  for (let i = 0; i < num_users; i++)
+  {
+    const accessToken = tokens[i]
+    const params = JSON.stringify({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    });
+    payload = {
+      groupId: id,
+        amount: 1
+    }
+    http.post(url, payload, params);
+    payload = {
+      groupId: id
+    }
+    http.post(url2, payload, params);
+  }
+}
+function save_unsave_test() {
+  const url = baseURL+'groupApi/saveGroup';
+  const url2 = baseURL+'groupApi/unSaveGroup';
+  for (let i = 0; i < num_users; i++)
+  {
+    const accessToken = tokens[i]
+    const params = JSON.stringify({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    });
+    payload = {
+      groupId: id
+    }
+    http.post(url, payload, params);
+    http.post(url2, payload, params);
+  }
+}
 /*export default () => {
   const accessToken = 'valid-token';
-  const url = 'http://132.73.84.56/groupApi/getPage';
+  const url = baseURL+'groupApi/getPage';
   const params = JSON.stringify({
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${accessToken}`,
@@ -57,7 +148,18 @@ function generate_many_users() {
   };
   http.get(url, payload, params);
 }*/
-export default () => {
+// uncomment to perform tests
+//test 1
+/*export default () => {
   generate_many_users();
-}
-//generate_users_file();
+}*/
+//test 2
+/*init_chat_test()
+export default () => {
+  chat_test();
+}*/
+//test 3
+/*mass_login()
+export default () => {
+  join_unjoin();
+}*/
